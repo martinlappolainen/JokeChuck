3b919944360eb7a3f47d4bc18ffc8f8cae9e4bb0
Opilane TTHK <opilane@tthk.ee>
2020-10-06 09:43:33 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.IO;
+using System.Net;
+
+namespace ChuckApi
+{
+    class Program
+    {
+        private static string url = "https://api.chucknorris.io/jokes/random";
+        static void Main(string[] args)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+
+            var WebResponse = request.GetResponse();
+            var webStream = WebResponse.GetResponseStream();
+
+            using (var responseReader = new StreamReader(webStream))
+            {
+                var response = responseReader.ReadToEnd();
+
+                Joke joke = JsonConvert.DeserializeObject<Joke>(response);
+                Console.WriteLine($"Joke:{joke.Value}");
+            }
+        }
+    }
+}
